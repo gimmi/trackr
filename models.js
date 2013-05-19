e160396d92661eb6f820a251c5eb3f6b9b8c6c63
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-05-19 19:37:46 +0200

    Configured mongoose-text-search plugin for querying on fulltext index

--- models.js
+++ models.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose'),
-	ObjectId = require('mongoose').Schema.Types.ObjectId;
+	ObjectId = require('mongoose').Schema.Types.ObjectId,
+	textSearchPlugin = require('mongoose-text-search');
 
 var options = { 
 	toJSON: { 
@@ -15,6 +16,10 @@ var itemSchema = mongoose.Schema({
 	body: String,
 	tags: [String]
 }, options);
+
+itemSchema.plugin(textSearchPlugin); // Query with Item.textSearch('ciao', function (err, ret) {});
+
+itemSchema.index({ tags: 'text', title: 'text', body: 'text' }, { name: 'itemTextIndex', weights: { tags: 10, title: 5, body: 1 } });
 
 exports.Item = mongoose.model('Item', itemSchema);
 
